229893e1d5c682ce5442859cf5fe25fef9552c57
Maks <makso544g@gmail.com>
2024-11-12 10:11:43 +0000

    Added passport.js with LocalStartegy for user authentication and expres-session for saving user sessions

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import pg from 'pg';
 import bcrypt from 'bcrypt';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import session from 'express-session';
 import 'dotenv/config';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -14,6 +17,19 @@ const PORT = process.env.PORT;
 
 // Setting up cors to listen to localhost requests
 app.use(cors());
+
+// Setting up session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Initializing passport.js for authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Setting up number of salt rounds for password hashing
 const saltRounds = 10;
@@ -62,7 +78,57 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', (req, res) => {});
+// Setting login route, authenticating user using passport.js
+app.post('/login', (req, res) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
+  });
+});
+
+// Setting up passport local strategy for user authentication using passport.js
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, async function verify(
+    email,
+    password,
+    cb
+  ) {
+    try {
+      const result = await db.query('SELECT * FROM users WHERE email = $1', [
+        email,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            console.log('Error comparing passwords:', err);
+            return cb(err);
+          } else {
+            if (valid) {
+              return cb(null, user);
+            } else {
+              return cb(null, false);
+            }
+          }
+        });
+      } else {
+        return cb('User not found');
+      }
+    } catch (error) {
+      console.log(err);
+      return cb(err);
+    }
+  })
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
 
 // Server listening on specified port
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
