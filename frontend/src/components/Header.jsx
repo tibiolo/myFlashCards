fdaca9d3493bc6494f9a7b2dd6f7eba6453ed7d6
tibiolo <makso544g@gmail.com>
2024-11-21 02:40:37 +0000

    Added logout dropdown box as well as axios reaching the logout backend route

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import AddIcon from '@mui/icons-material/Add';
-import { Avatar } from '@mui/material';
-import '../styles/header.css';
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Avatar, Menu, MenuItem } from "@mui/material";
+import axios from "axios";
+import "../styles/header.css";
 
-import Button from './Button';
-import { useNavigate } from 'react-router-dom';
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleNavigate = () => {
-    navigate('/create_collection');
+    navigate("/create_collection");
+  };
+
+  const handleAvatarClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = (event) => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/logout");
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+
+    handleMenuClose();
+    navigate("/login");
   };
 
   return (
@@ -21,7 +43,27 @@ function Header() {
         className="add-collection-btn"
         onClick={handleNavigate}
       />
-      <Avatar className="user-avatar" />
+      <div className="avatar-container">
+        <Avatar className="user-avatar" onClick={handleAvatarClick} />
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          className="dropdown-menu"
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem onClick={handleLogout} className="dropdown-item">
+            Logout
+          </MenuItem>
+        </Menu>
+      </div>
     </header>
   );
 }
