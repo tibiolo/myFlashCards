ccad2b89aabcc5ac9b2183308239fc0ac2d70a3b
tibiolo <makso544g@gmail.com>
2024-12-01 23:48:57 +0000

    Added a Collection component

---
+++ frontend/src/components/Collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Collection(props) {
+  return (
+    <div className="card" onClick={props.onClick} >
+      <h2 className="card-title">{props.title}</h2>
+      <p className="card-description">{props.description}</p>
+    </div>
+  )
+}
+
+export default Collection;
